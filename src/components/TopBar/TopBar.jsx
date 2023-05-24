554b3c4bf5317df7b6302236c0ba4d4db91704ef
ChenElla <cyqfypex@gmail.com>
2023-05-24 00:11:42 -0400

    Brighter version

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -2,7 +2,7 @@ import profile_icon from '../../assets/icons/ProfileMajor.svg';
 import setting_icon from '../../assets/icons/SettingsMajor.svg';
 import logout_icon from '../../assets/icons/LogOutMinor.svg';
 import { useState } from 'react';
-import logo from '../../assets/logo/shopify_logo_darkbg.svg';
+import logo from '../../assets/logo/shopify_logo_whitebg.svg';
 import user_icon from '../../assets/user_icons/sophie.png';
 import back_icon from '../../assets/icons/CircleLeftMajor.svg';
 import PropTypes from 'prop-types';
@@ -12,14 +12,14 @@ TopBar.propTypes = {
 export default function TopBar({setMain}) {
     const [open, setOpen] = useState(false);
   return (
-    <div className = "absolute top-0 flex flex-row justify-between items-baseline w-full bg-green-900 shadow-xl text-white">
+    <div className = "absolute top-0 flex flex-row justify-between items-baseline w-full bg-white shadow-xl text-black">
         <div className = "flex flex-row justify-start items-center p-4 hover:cursor-pointer" onClick = {()=>setMain('posts')}>
             <a href= "https://community.shopify.com/c/shopify-community/ct-p/en"><img className = "w-6 h-6 brightness-200 hover:brightness-0 mr-3" src={back_icon} alt="back_to_community" /></a>
             <div className = "flex flex-row items-center justify-start">
-            <img className = "h-12" src= {logo} alt="shopifyHub_logo"/><span className = "italic text-4xl font-thin text-white">hub</span>
+            <img className = "h-12" src= {logo} alt="shopifyHub_logo"/><span className = "italic text-4xl font-thin text-black">hub</span>
             </div>
         </div>
-        <div className = "flex flex-row items-center text-white text-base justify-between w-1/2">
+        <div className = "flex flex-row items-center text-black text-base justify-between w-1/2">
             <a className = "hover:underline" href="https://apps.shopify.com/">My Shopify Store</a>
             <a className = "hover:underline" href="https://community.shopify.com/c/shopify-community/ct-p/en">Shopify Community</a>
             <a className = "hover:underline" href="https://academy.shopify.com/">Shopify Academy</a>
