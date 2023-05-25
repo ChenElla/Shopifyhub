572926013f916e9e229ac2c8c64cd168f17307a7
ChenElla <cyqfypex@gmail.com>
2023-05-25 14:17:40 -0400

    Update the cursor shown when hovering for channel lists

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -55,11 +55,11 @@ export default function List({ about, header_context, setChannel, setMain }) {
   }
   return (
     <div className="list_column mb-2 ml-0 mr-0 box-border">
-      <div className="relative flex flex-row justify-between items-center mb-2 hover:cursor-help">
+      <div className="relative flex flex-row justify-between items-center mb-2">
         <div className="flex flex-row justify-start items-center underline">
           <img className="w-5 h-5 mr-2" src={header_icon} alt="header_icon" />
           <div
-            className="text-lg font-semibold"
+            className="text-lg font-semibold hover:cursor-help"
             onMouseOver={() => setInstructionOn(true)}
             onMouseOut={() => setInstructionOn(false)}
             alt="what does this column do?"
